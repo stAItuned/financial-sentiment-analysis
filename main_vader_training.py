c1bc866d4023a2ffcf1bd936e01e42c02a2f0424
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-06 22:56:58 +0200

    Update Vader

---
+++ main_vader_training.py
@@ -0,0 +1,38 @@
+import logging
+from typing import Text, Dict
+
+from constants.config import TWITTER_DATASET, VADER
+from constants.paths import RESULT_DIR
+from scripts.pipelines.training_pipeline_unsupervised import model_training
+
+logging.basicConfig(level='INFO')
+logger = logging.getLogger()
+
+
+def main(model_name: Text,
+         data_params: Dict,
+         model_params: Dict,
+         seed: int,
+         save_dir: Text = None):
+
+    model_training(model_name, data_params, model_params, seed)
+
+
+if __name__ == '__main__':
+    model_name = VADER
+    seed = 2021
+
+    data_params = {'data_path': 'resources/twitter_dataset/TSLA.csv',
+                   'dataset_type': TWITTER_DATASET,
+                   'preprocessed': False,
+                   'vectorization': None,
+                   'vector_params': None,
+                   'imbalance': None,
+                   'imb_params': None,
+                   'test_size': 0.99,
+                   'shuffle': False,
+                   'train': True}
+
+    model_params = {}
+
+    main(model_name, data_params, model_params, seed, RESULT_DIR)
