6a0308b1c65fa21427a0fb6e040773eaf9d80b87
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-06 21:22:59 +0200

    Update text_preprocessing
    - remove_html()
    - remove_links()
    - normalize_punctuation()
    - normalize_whitespaces()
    - normalize_contractions()
    - normalize_char_sequences()
    - clean_twitter()

--- core/preprocessing/text_preprocessing.py
+++ core/preprocessing/text_preprocessing.py
@@ -1,9 +1,14 @@
 from nltk.corpus import stopwords
+from nltk import word_tokenize
+import json
 import nltk
 import string
+import re
 
 porter = nltk.PorterStemmer()
 lemmatizer = nltk.WordNetLemmatizer()
+
+contractions_dictionary = json.loads(open('resources/dictionaries/english_contractions.json', 'r').read())
 
 
 def init_nltk():
@@ -20,7 +25,6 @@ def remove_punctuations(text):
 
 def remove_stopwords(sentence, lang='english'):
     """
-
     :param sentence:        str
     :param lang:            str ['english' | 'italian' ]
 
@@ -33,6 +37,126 @@ def remove_stopwords(sentence, lang='english'):
     for word in sentence.split(sep=' '):
         if word not in stop_words:
             new_sentence += f'{word} '
+
+    return new_sentence
+
+def remove_html(sentence):
+    """
+    :param sentence:        str
+
+    :return new_sentence:   str  --> sentence without html tags
+    """
+
+    # . : any character
+    # *? : 0 or more, ungreedy
+    new_sentence = re.sub("<.*?>", '', sentence).strip()
+
+    return new_sentence
+
+def remove_links(sentence):
+    """
+    :param sentence:        str
+
+    :return new_sentence:   str  --> sentence without links
+    """
+
+    # \S : not withe space
+    # + : 1 or more
+    # |-- it will cover both http and https
+    new_sentence = re.sub("http\S+", '', sentence).strip()
+
+    return new_sentence
+
+def normalize_punctuation(sentence):
+    """
+    :param sentence:        str
+
+    :return new_sentence:   str  --> sentence with a normalized punctuation
+                                     e.g. !!!! -> !
+                                          ??!! -> ?!
+                                          ..., .., ...., -> ...
+    """
+    # extract each group of the same punctuation and maintain just the first match
+    new_sentence = re.sub(r'([!?,;])\1+', r'\1', sentence)
+    # each combination of dots (with two or more) will be normalized to three dots
+    new_sentence = re.sub(r'\.{2,}', r'...', new_sentence)
+
+    return new_sentence
+
+def normalize_whitespaces(sentence):
+    """
+    :param sentence:        str
+
+    :return new_sentence:   str  --> sentence with normalized spaces
+    """
+
+    # normalize one or more "spaces" into just a single space
+    new_sentence = re.sub(r"( )\1+", r"\1", sentence)
+    # normalize one or more "new lines" to just a single new line
+    new_sentence = re.sub(r"(\n)\1+", r"\1", new_sentence)
+    # normalize one or more "carriage returns" to just a single carriage return
+    new_sentence = re.sub(r"(\r)\1+", r"\1", new_sentence)
+    # normalize one or more "tabs" to just a single tab
+    new_sentence = re.sub(r"(\t)\1+", r"\1", new_sentence)
+
+    return new_sentence
+
+def normalize_contractions(sentence,lang='english'):
+    """
+    :param sentence:        str
+    :param lang:            str ['english' | 'italian' ]
+                                 --> not implemented yet for italian
+
+    :return new_sentence:   str  --> sentence with normalized contractions
+                                     we use a dictionary provided by Wikipedia
+    """
+
+    if lang == "english":
+        tokens = word_tokenize(sentence)
+
+        for i, token in enumerate(tokens):
+            if token in contractions_dictionary:
+                tokens[i] = contractions_dictionary[token]
+
+        new_sentence = ' '.join(tokens)
+        return new_sentence
+
+    # to do : manage italian contractions
+    elif lang == "italian":
+        return sentence
+
+
+def normalize_char_sequences(sentence):
+    """
+    :param sentence:        str
+
+    :return new_sentence:   str  --> sentence with normalized char sequences
+                                     dropped to the second repetition
+                                     e.g. Helllloooo -> Helloo
+                                          Byeeee -> Byee
+                                          Yeeees -> Yees
+    """
+
+    # take all the characters repeated from 2 to "n" times
+    # replace them just with the first two chars
+    new_sentence = re.sub(r'(.)\1{2,}', r'\1\1', sentence)
+
+    return new_sentence
+
+def clean_twitter(sentence):
+    """
+    :param sentence:        str
+
+    :return:                str --> sentence without #, mentions and RT
+    """
+
+    # remove retweets
+    # e.g. "RT @first_user"
+    new_sentence = re.sub(r'RT( )@[A-z0-9]{1,}', '', sentence).strip()
+
+    # remove "#" -> the text associated to the hashtag may contain useful informantions
+    # remove non alphabetical chars
+    new_sentence = re.sub("[^a-zA-Z]", " ", new_sentence).strip()
 
     return new_sentence
 
