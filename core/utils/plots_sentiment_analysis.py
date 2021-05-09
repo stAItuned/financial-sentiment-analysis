33b03e382ffc7e6859d6c6b4300910656767eca1
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-09 23:38:50 +0200

    Sentiment analysis graphs

--- core/utils/plots_sentiment_analysis.py
+++ core/utils/plots_sentiment_analysis.py
@@ -1,5 +1,7 @@
 import plotly.express as px
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from collections import Counter
 
@@ -9,13 +11,33 @@ from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+
+def getOrderedDictionary(data):
+    """
+    :param data: (Series)       --> sentences
+
+    :return: (string, integer)  --> list with the words and
+                                    list with their frequencies
+    """
+    dict_sentences = {}
+
+    for sentence in data:
+        tokens = word_tokenize(sentence)
+        for token in tokens:
+            if token in dict_sentences and token not in stopwords.words('english'):
+                dict_sentences[token] += 1
+            else:
+                dict_sentences[token] = 1
+
+    ordered_dict = {k: v for k, v in sorted(dict_sentences.items(), key=lambda item: item[1], reverse=True)}
+
+    return list(ordered_dict.keys()), list(ordered_dict.values())
 
 
 def get_dic_sentiment(labels):
     """
     :param labels: (Series) --> predictions of the sentiment analysis
+
     :return: (dic)          --> dictionary with the number of positive, negative and neutral values
     """
     dic_sentiment = dict(Counter(labels))
@@ -27,47 +49,76 @@ def get_dic_sentiment(labels):
     return dic_sentiment
 
 
+def filterQuantiles(data, confidence):
+    """
+    :param data: (dataframe)     --> dataframe containing the phrases and
+                                     their sentiment
+    :param confidence: (double)  --> confidence of the length of distribution
+                                     in order to remove outliers
+
+    :return: (lists)             --> list with the length values for pos/neg/neutral values
+    """
+    x_neg = data[data['sentiment'] == -1]["length"]
+    x_neutr = data[data['sentiment'] == 0]["length"]
+    x_pos = data[data['sentiment'] == 1]["length"]
+
+    # remove the values outiside the 2.5th and 97.5th percentile
+    # avoid long tail
+    lower_quantile = (100 - confidence) / 2
+    upper_quantile = (100 - lower_quantile)
+
+    x_neg_cleaned = [x for x in x_neg if x >= np.percentile(x_neg, lower_quantile) and \
+                     x <= np.percentile(x_neg, upper_quantile)]
+
+    x_neutr_cleaned = [x for x in x_neutr if x >= np.percentile(x_neutr, lower_quantile) and \
+                       x <= np.percentile(x_neutr, upper_quantile)]
+
+    x_pos_cleaned = [x for x in x_pos if x >= np.percentile(x_pos, lower_quantile) and \
+                     x <= np.percentile(x_pos, 97.5)]
+
+    return x_neg_cleaned, x_neutr_cleaned, x_pos_cleaned
+
+
 def plot_piechart(labels_twitter, labels_yahoo):
     """
-    :param labels_twitter:
-    :param labels_yahoo:
-    :return:
+    :param labels_twitter: (series) --> twitter sentiment
+    :param labels_yahoo:   (series) --> yahoo sentiment
+
+    :return: (plt)
     """
     dic_sentiment_twitter = get_dic_sentiment(labels_twitter)
     dic_sentiment_yahoo = get_dic_sentiment(labels_yahoo)
 
-    colors_twitter = ['#03045e', '#00b4d8', '#caf0f8']
-    colors_yahoo = ['#480ca8', '#7209b7', '#f72585']
+    #colors_twitter = ['#03045e', '#00b4d8', '#caf0f8']
+    #colors_yahoo = ['#480ca8', '#7209b7', '#f72585']
 
-    fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+    fig = make_subplots(rows=1, cols=2,
+                        specs=[[{'type':'domain'}, {'type':'domain'}]],
+                        subplot_titles=("Twitter", "Yahoo"))
 
     fig.add_trace(go.Pie(labels=list(dic_sentiment_twitter.keys()),
                          values=list(dic_sentiment_twitter.values()),
                          name="TWITTER",
-                         marker_colors=colors_twitter,
+                         #marker_colors=colors_twitter,
                          ),
                   1, 1)
 
     fig.add_trace(go.Pie(labels=list(dic_sentiment_yahoo.keys()),
                          values=list(dic_sentiment_yahoo.values()),
                          name="YAHOO",
-                         marker_colors=colors_yahoo
+                         #marker_colors=colors_yahoo
                          ),
 
                   1, 2)
 
     fig.update_traces(hoverinfo='label+percent+name')
-    fig.update(layout_title_text='SENTIMENT ANALYSIS - TWITTER VS YAHOO',
-               layout_showlegend=False)
+    fig.update(layout_title_text='SENTIMENT ANALYSIS',
+               layout_showlegend=True)
 
     return fig
 
 
-#####################################
-### FIX
-#####################################
-
-def plot_most_frequent(data, k=10, dataset="TWITTER"):
+def plot_most_frequent(data_twitter, data_yahoo, k=10):
     """
     :param data: (Series) --> text of the tweets/headlines
     :param k: (int)       --> number of top words to show
@@ -76,84 +127,82 @@ def plot_most_frequent(data, k=10, dataset="TWITTER"):
     :return: plot of the top k elements (most frequents)
     """
 
-    if dataset == "twitter_data":
-        color_palette = px.colors.sequential.Teal_r
-        label_title = "TWITTER"
-    elif dataset == "YAHOO":
-        color_palette = px.colors.sequential.BuPu_r
-        label_title = "YAHOO"
+    palette_twitter = px.colors.sequential.Teal_r
+    palette_yahoo = px.colors.sequential.BuPu_r
 
-    dict_words = {}
+    keys_twitter, values_twitter = getOrderedDictionary(data_twitter)
+    keys_yahoo, values_yahoo = getOrderedDictionary(data_yahoo)
 
-    for sentence in data:
-        tokens = word_tokenize(sentence)
-        # already removed the stopwords
-        for token in tokens:
-            if token in dict_words and token not in stopwords.words('english'):
-                dict_words[token] += 1
-            else:
-                dict_words[token] = 1
+    fig = make_subplots(rows=1, cols=2,
+                        subplot_titles=("Twitter", "Yahoo"))
 
-    ordered_dict = {k: v for k, v in sorted(dict_words.items(), key=lambda item: item[1], reverse=True)}
+    fig.add_trace(go.Bar(x=values_twitter[:k][::-1], y=keys_twitter[:k][::-1], orientation='h'
+                         #marker=dict(color=[4, 5, 6], coloraxis="coloraxis")
+                         ),
+                  1, 1)
 
-    keys = list(ordered_dict.keys())[:k]
-    values = list(ordered_dict.values())[:k]
+    fig.add_trace(go.Bar(x=values_yahoo[:k][::-1], y=keys_yahoo[:k][::-1],orientation='h'
+                         # marker=dict(color=[4, 5, 6], coloraxis="coloraxis")
+                         ),
+                  1, 2)
 
-    fig = px.bar(x=values, y=keys,
-
-                 category_orders={  # replaces default order by column name
-                     "y": [y for x, y in sorted(zip(values, keys), reverse=True)]
-                 },
-
-                 title=f"MOST FREQUENT WORDS - {label_title}",
-                 color_discrete_sequence=color_palette,
-
-                 labels={
-                     "x": "COUNTER",
-                     "y": "WORD"
-                 })
+    fig.update_layout(title="MOST FREQUENT WORDS")
 
     return fig
 
 
-def plot_length_distributions(data, labels, dataset):
+def plot_length_distributions(data_t, labels_t, data_y, labels_y):
     """
-    :param data:    (df)  --> complete dataframe with text and prediction
-                              # see how can be merged, otherwise merge here
-    :param dataset: (str) --> type of input data (twitter/yahoo)
+    :param data_t: (dataframe) --> twitter dataframe
+    :param labels_t: (series)  --> twitter sentiment
+    :param data_y: (dataframe) --> yahoo dataframe
+    :param labels_y: (series)  --> yahoo sentiment
+
+    :return: (graph)          --> length distribution
     """
-    data = pd.DataFrame(data)
-    data['length'] = data.Phrase.apply(lambda text: len(text))
-    data['sentiment'] = labels
+    fig = make_subplots(subplot_titles=('Twitter', 'Yahoo'),
+                        cols=2, rows=2,
+                        vertical_spacing=0.1,
+                        horizontal_spacing=0.1
+                        )
 
-    x_neg = data[data['sentiment'] == -1]["length"]
-    x_neutr = data[data['sentiment'] == 0]["length"]
-    x_pos = data[data['sentiment'] == 1]["length"]
+    # twitter
+    data_t = pd.DataFrame(data_t)
+    data_t['length'] = data_t.Phrase.apply(lambda text: len(text))
+    data_t['sentiment'] = labels_t
 
-    # remove the values outiside the 2.5th and 97.5th percentile
-    # avoid long tail
-    x_neg_cleaned = [x for x in x_neg if x >= np.percentile(x_neg, 2.5) and \
-                     x <= np.percentile(x_neg, 97.5)]
+    twitter_neg, twitter_neutr, twitter_pos = filterQuantiles(data_t, 95)
+    twitter_hist = [twitter_neg, twitter_neutr, twitter_pos]
 
-    x_neutr_cleaned = [x for x in x_neutr if x >= np.percentile(x_neutr, 2.5) and \
-                       x <= np.percentile(x_neutr, 97.5)]
+    # yahoo
+    data_y = pd.DataFrame(data_y)
+    data_y['length'] = data_y.Phrase.apply(lambda text: len(text))
+    data_y['sentiment'] = labels_y
 
-    x_pos_cleaned = [x for x in x_pos if x >= np.percentile(x_pos, 2.5) and \
-                     x <= np.percentile(x_pos, 97.5)]
+    yahoo_neg, yahoo_neutr, yahoo_pos = filterQuantiles(data_y, 95)
+    yahoo_hist = [yahoo_neg, yahoo_neutr, yahoo_pos]
 
-    hist_data = [x_neg_cleaned, x_neutr_cleaned, x_pos_cleaned]
     group_labels = ["negative", "neutral", "positive"]
 
-    if dataset == "twitter_data":
-        colors = ['#03045e', '#00b4d8', '#caf0f8']
-        label_title = "TWITTER"
-    elif dataset == "YAHOO":
-        colors = ['#480ca8', '#7209b7', '#f72585']
-        label_title = "YAHOO"
+    colors_twitter = ['#03045e', '#00b4d8', '#caf0f8']
+    colors_yahoo = ['#480ca8', '#7209b7', '#f72585']
 
-    fig = ff.create_distplot(hist_data, group_labels, colors=colors, bin_size=10,
-                             show_curve=False)
+    # plots
+    fig_twitter = ff.create_distplot(twitter_hist, group_labels, colors=colors_twitter)
+    fig_yahoo = ff.create_distplot(yahoo_hist, group_labels, group_labels, colors=colors_yahoo)
 
-    fig.update_layout(title_text=f'LENGTH DISTRIBUTION - {label_title}')
+    distplot_left = fig_twitter['data']
+    distplot_right = fig_yahoo['data']
+
+    for i in range(6):
+        if i <= 2:
+            fig.append_trace(distplot_left[i], 1, 1)
+            fig.append_trace(distplot_right[i], 1, 2)
+
+        else:
+            fig.append_trace(distplot_left[i], 2, 1)
+            fig.append_trace(distplot_right[i], 2, 2)
+
+    fig.update_layout(title_text=f'LENGTH DISTRIBUTION', autosize=False, height=700)
 
     return fig
