b30884f94aeb54cb4f356e8d7d32d1348851af2a
daniele21 <danielemoltisanti@live.it>
2021-05-10 18:04:37 +0200

    nn model training phase

--- scripts/networks/conv_lstm_network.py
+++ scripts/networks/conv_lstm_network.py
@@ -1,10 +1,11 @@
 import logging
+from typing import Dict
 
 import torch
 from torch import nn
 from torch.nn import functional as F
 
-from constants.config import MAX_WORD_SENTENCE
+from constants.config import MAX_WORD_SENTENCE, TDIDF_EMBEDDING, TOKENIZER
 from scripts.networks.network_class import MyNetwork
 
 logger = logging.getLogger(__name__)
@@ -19,16 +20,14 @@ class Conv1D_Network(MyNetwork):
     def __init__(self, params):
         super().__init__(params)
 
-        self.embedding = nn.Embedding(num_embeddings=params['n_words'],
-                                      embedding_dim=params['emb_dim'],
-                                      padding_idx=0)
+        self.embedding = init_embeddings(params)
 
         self.conv_layers = generate_conv_1D(params)
         self.max_pooling = nn.MaxPool1d(kernel_size=params['pooling_kernel'])
 
         self.dropout = nn.Dropout(params['dropout'])
 
-        self.fc = nn.Linear(in_features=compute_fc_in_features(MAX_WORD_SENTENCE, params),
+        self.fc = nn.Linear(in_features=compute_fc_in_features(params),
                             out_features=1)
 
     def forward(self, x):
@@ -36,12 +35,18 @@ class Conv1D_Network(MyNetwork):
         logger.debug(f' Forwarding {self.__class__.__name__}')
         logger.debug(f' \t> Input shape: {x.shape}')
 
-        emb = self.embedding(x)
-        emb = emb.permute(0, 2, 1)
-        logger.debug(f' \t> Emb shape: {emb.shape}')
+        if self.embedding is not None:
+            emb = self.embedding(x)
+            logger.debug(f' \t> Emb shape: {emb.shape}')
+            dropout_emb = self.dropout(x)
+            x = dropout_emb
+        else:
+            x = x.unsqueeze(1)
 
-        dropout_emb = self.dropout(emb)
-        convs = [F.relu(conv(dropout_emb)) for conv in self.conv_layers]
+        logger.debug(f' \t> Shape: {x.shape}')
+        # x = x.permute(0, 2, 1)
+
+        convs = [F.relu(conv(x)) for conv in self.conv_layers]
         logger.debug(f' \t> Conv shape: {convs[0].shape}')
 
         max_pools = [self.max_pooling(conv).squeeze() for conv in convs]
@@ -50,13 +55,25 @@ class Conv1D_Network(MyNetwork):
         concat = torch.cat(max_pools, dim=1)
         logger.debug(f' \t> Concat shape: {concat.shape}')
 
-        flatten = torch.reshape(concat, (concat.shape[0], concat.shape[1]*concat.shape[2]))
+        flatten = torch.reshape(concat, (concat.shape[0], concat.shape[1] * concat.shape[2]))
         logger.debug(f' \t> Flatten shape: {flatten.shape}')
 
         out = torch.sigmoid(self.fc(flatten))
         logger.debug(f' \t> Out shape: {out.shape}')
 
         return out
+
+
+def init_embeddings(params: Dict):
+    if params['dataset_type'] == TDIDF_EMBEDDING:
+        return None
+
+    elif params['dataset_type'] == TOKENIZER:
+        return nn.Embedding(num_embeddings=params['n_words'],
+                            embedding_dim=params['emb_dim'],
+                            padding_idx=0)
+    else:
+        raise AttributeError(f'No valid embedding type found: {params["dataset_type"]}')
 
 
 def generate_conv_1D(params):
@@ -70,7 +87,8 @@ def generate_conv_1D(params):
 
     if isinstance(kernels, int):
         kernel = kernels
-        conv_layers.append(nn.Conv1d(in_channels=params['emb_dim'],
+        in_channels = 1 if params['dataset_type'] == TDIDF_EMBEDDING else params['emb_dim']
+        conv_layers.append(nn.Conv1d(in_channels=in_channels,
                                      out_channels=out_channels,
                                      kernel_size=kernel,
                                      stride=stride,
@@ -81,8 +99,8 @@ def generate_conv_1D(params):
             conv_layers.append(nn.Conv1d(in_channels=params['emb_dim'],
                                          out_channels=out_channels,
                                          kernel_size=kernel),
-                                         stride=stride,
-                                         padding=padding)
+                               stride=stride,
+                               padding=padding)
 
     else:
         raise TypeError(f'No valid input for kernels: {kernels}')
@@ -90,13 +108,13 @@ def generate_conv_1D(params):
     return conv_layers
 
 
-def compute_fc_in_features(input_length, params):
-
+def compute_fc_in_features(params):
     out_channels = params['out_channels']
     kernel_size = params['kernel_size']
     pooling_kernel = params['pooling_kernel']
     stride = params['stride']
+    emb_dim = params['emb_dim']
 
-    in_features = ((input_length // stride) // pooling_kernel) * out_channels
+    in_features = ((emb_dim // stride) // pooling_kernel) * out_channels
 
     return in_features
