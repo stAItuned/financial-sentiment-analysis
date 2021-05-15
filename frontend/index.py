06f88e74a9b4b9966b3ffa16fd8a481db877de6c
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-15 18:10:06 +0200

    Update fronted - main page

--- frontend/index.py
+++ frontend/index.py
@@ -1,7 +1,34 @@
 import streamlit as st
+import datetime
+
+from core.utils.financial_data_utils import get_data,create_summary
+
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 def app():
 
-    st.title('PAGE 1')
+        st.title('HOME')
 
-    st.markdown(' test for page 1 ')
+        label_obj = st.empty()
+        slider_obj = st.empty()
+
+        ticker_input = label_obj.text_input("Select ticker", 'AAPL')
+        value = slider_obj.slider("Select the number of months: ", 4, 60, 24, 1)
+
+        today = datetime.date.today()
+        start_date = today - datetime.timedelta(days=value * 30)
+        history = get_data(ticker_input, start_date)
+        history_close = history['Adj Close']
+
+        fig = go.Figure(data=[go.Candlestick(x=history.index, open=history.Open,
+                                             high=history.High, low=history.Low,
+                                             close=history.Close)])
+
+        # responsive layout
+        st.plotly_chart(fig, use_container_width=True)
+
+        # summary
+        st.dataframe(create_summary(history_close))
+
