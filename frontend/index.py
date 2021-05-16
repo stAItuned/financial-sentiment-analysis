17455d7e0a594bda6103e970b18d61cee019c2b0
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-16 23:04:17 +0200

    Stock comparison plot

--- frontend/index.py
+++ frontend/index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 
-from core.utils.financial_data_utils import get_data,create_summary
+from core.utils.financial_data_utils import *
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -22,13 +22,21 @@ def app():
         history = get_data(ticker_input, start_date)
         history_close = history['Adj Close']
 
-        fig = go.Figure(data=[go.Candlestick(x=history.index, open=history.Open,
+        fig_candlestick = go.Figure(data=[go.Candlestick(x=history.index, open=history.Open,
                                              high=history.High, low=history.Low,
                                              close=history.Close)])
 
         # responsive layout
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig_candlestick, use_container_width=True)
 
         # summary
+        st.markdown("<h3> SUMMARY </h3>", unsafe_allow_html=True)
         st.dataframe(create_summary(history_close))
 
+        # comparison
+        st.markdown("<h3> BENCHMARK COMPARISON </h3>", unsafe_allow_html=True)
+        sector, similar_companies = get_sector(ticker_input)
+        selected_companies = st.multiselect('Compare it with other companies on the same sector on the S&P500',
+                                             similar_companies)
+
+        st.plotly_chart(plot_sector_companies(ticker_input, start_date, selected_companies), use_container_width=True)
