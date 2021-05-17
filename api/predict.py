d4f93d63cd94eedd9093348fe6525efe918dc330
daniele21 <danielemoltisanti@live.it>
2021-05-17 16:43:59 +0200

    api init

---
+++ api/predict.py
@@ -0,0 +1,20 @@
+from flask import make_response, jsonify, request
+from flask import current_app as app
+
+from scripts.models.utils import init_model
+
+
+@app.route('/predict/sentiment', methods=['POST'])
+def predict_sentiment():
+    data = request.json
+    sentence = data['sentence']
+    model_name = data['model']
+    model_params = data.get('params')
+
+    model = init_model(model_name)(model_name, model_params)
+    sentiment = model.predict(sentence)
+
+    return make_response(jsonify({'sentence': sentence,
+                                  'sentiment': sentiment}))
+
+
