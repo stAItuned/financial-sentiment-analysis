88bf77dd6aa04dbcb1aa458c1ed3d94b780f5e5b
daniele21 <danielemoltisanti@live.it>
2021-05-18 20:49:46 +0200

    fix zero_grad

--- scripts/models/transformer_model.py
+++ scripts/models/transformer_model.py
@@ -8,6 +8,9 @@ from transformers import pipeline
 
 logger = logging.getLogger()
 
+POSITIVE_BOUND = 0.9
+NEGATIVE_BOUND = -0.9
+
 
 class Transformer_Model(Model):
 
@@ -40,14 +43,18 @@ class Transformer_Model(Model):
         return prediction
 
     def postprocessing(self, x):
-        post_pred = []
-        for value in x:
-            if value > 0.9:
-                post_pred.append(1)
-            elif value < -0.9:
-                post_pred.append(-1)
-            else:
-                post_pred.append(0)
+
+        if isinstance(x, List):
+            post_pred = []
+            for value in x:
+                if value > POSITIVE_BOUND:
+                    post_pred.append(1)
+                elif value < NEGATIVE_BOUND:
+                    post_pred.append(-1)
+                else:
+                    post_pred.append(0)
+        else:
+            post_pred = 1 if x > POSITIVE_BOUND else -1 if x < NEGATIVE_BOUND else 0
 
         return post_pred
 
