23ee4f0a6f1f02f8fa9045c661dbcf757f64823a
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-19 17:50:27 +0200

    Plot - Sentiment and Stock trends

--- core/utils/plots_sentiment_analysis.py
+++ core/utils/plots_sentiment_analysis.py
@@ -15,6 +15,8 @@ import datetime as dt
 
 import streamlit as st
 
+import yfinance as yf
+
 
 def getOrderedDictionary(data):
     """
@@ -270,3 +272,45 @@ def plot_length_distributionsV2(data_t, labels_t, data_y, labels_y):
                        )
 
     return fig
+
+def plot_sentiment_trend(df, labels, ticker):
+    """
+    :param df: (DataFrame) news and respective dates
+    :param labels: (list) predicted sentiment
+    :param ticker: (str) ticker of the stock
+    :return: (Figure) comparison among sentiment and price trends
+    """
+
+    fig = make_subplots(2, 1, row_heights=[0.7, 0.3])
+
+    # add the labels on a new 'sentiment' column
+    df['sentiment'] = labels
+    # filter just positive and negative news
+    df_no_neutral = df[df['sentiment'] != 0].copy()
+
+    # since we're showing the daily trend, we group by day
+    # we count the number of positive and negative news for each day
+    # to do so, we use three new columns
+    # |-- day -> feature on which we have to group
+    # |-- positive -> 1 if the news is positive, 0 otherwise
+    # |-- negative -> -1 if the news is negative, 0 otherwise
+    df_no_neutral['day'] = df_no_neutral.time.apply(lambda date: date.split(" ")[0])
+    df_no_neutral['positive'] = df_no_neutral.sentiment.apply(lambda s: 1 if s == 1 else 0)
+    df_no_neutral['negative'] = df_no_neutral.sentiment.apply(lambda s: -1 if s == -1 else 0)
+    aggregated = df_no_neutral.groupby('day').sum()[['positive', 'negative']]
+
+    # extract the stock prices according to the time interval available
+    # convert the string into dates
+    # to do : try with pandas
+    dates = [dt.datetime.strptime(date.split(".")[0], '%Y-%m-%d') for date in df_no_neutral.day]
+    data_ticker = yf.download('AAPL', start=min(dates), end=max(dates))
+    data_ticker_close = data_ticker['Adj Close']
+
+    fig.add_trace(go.Scatter(x=data_ticker_close.index, y=data_ticker_close.values, name=ticker), 1, 1)
+
+    fig.add_trace(go.Bar(x=aggregated.index, y=aggregated.positive, marker_color='green', name="positive"), 2, 1)
+    fig.add_trace(go.Bar(x=aggregated.index, y=aggregated.negative, marker_color='red', name="negative"), 2, 1)
+    fig.update_layout(barmode='relative', title_text=ticker)
+
+    return fig
+
