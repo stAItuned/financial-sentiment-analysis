23626e3d9f326bed136bfc3f2b008f5f2c0561f3
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-05-19 21:31:33 +0200

    Update index graphs

--- core/utils/financial_data_utils.py
+++ core/utils/financial_data_utils.py
@@ -16,29 +16,32 @@ def get_data(ticker, start):
     return yf.download(ticker, start=start)
 
 
-def create_summary(history_close):
+def create_summary(history):
     """
-    :param history_close:   historical close prices for the selected ticker
-    :return:                dataframe with a short summary of the price and
-                            volumes over the whole period
+    :param history_close: (DataFrame)  historical prices and volume
+    :return:  (DataFrame)        Summary on the changes over time
     """
 
-    initial_price = round(history_close[0], 2)
-    final_price = round(history_close[len(history_close) - 1], 2)
-    gain_loss = round(100 * (final_price - initial_price) / initial_price, 2)
+    initial_price = round(history['Adj Close'][0], 2)
+    final_price = round(history['Adj Close'][-1], 2)
+    gain_price = round(100 * (final_price - initial_price) / initial_price, 2)
 
-    df = pd.DataFrame({'_': ['price'],
-                       'start - $': [initial_price],
-                       'close - $': [final_price],
-                       'gain  /  loss - %': [gain_loss],
-                       'other 1 ': [' / '],
-                       'other  2': [' / ']})
+    initial_volume = round(history['Volume'][0], 2)
+    final_volume = round(history['Volume'][-1], 2)
+    gain_volume = round(100 * (final_volume - initial_volume) / initial_volume, 2)
+
+    df = pd.DataFrame({'_': ['Price', 'Volume'],
+                       f'    {str(history.index[0]).split(" ")[0]} ': [initial_price, initial_volume],
+                       f'    {str(history.index[-1]).split(" ")[0]} ': [final_price, final_volume],
+                       '    Gain / Loss - % ': [gain_price, gain_volume]
+                       })
+
     df = df.set_index('_')
 
     return df
 
 
-def get_sector(ticker):
+def get_ticker_list():
     """
     :param ticker: (String) ticker that we want to track
     :return: (String) sector of the company
@@ -49,11 +52,7 @@ def get_sector(ticker):
     # because of streamlit we're on the main director
     df = pd.read_csv('resources/dictionaries/S&P-Sectors.csv').set_index('Symbol')
 
-    sector = df.loc[ticker]['GICS Sector']
-    # similar_tickers = list(df[df['GICS Sector'] == sector].index)
-    # similar_companies = list(df[df['Security'] == sector].index)
-
-    return sector, df
+    return df
 
 
 def plot_sector_companies(ticker, date, other_companies, mapping):
@@ -65,13 +64,15 @@ def plot_sector_companies(ticker, date, other_companies, mapping):
     """
     # selected company
     main_history = get_data(ticker, date)
+    company_name = mapping.loc[ticker]['Security']
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=main_history.index, y=main_history['Adj Close'], name=ticker))
+    fig.add_trace(go.Scatter(x=main_history.index, y=main_history['Adj Close'], name=f"{ticker} - {company_name}"))
 
     for company in other_companies:
         ticker = mapping[mapping['Security'] == company].index[0]
         curr_history = get_data(ticker, date)
         fig.add_trace(go.Scatter(x=curr_history.index, y=curr_history['Adj Close'], name=f"{ticker} - {company}"))
+    fig.update_layout(showlegend=True)
 
     return fig
