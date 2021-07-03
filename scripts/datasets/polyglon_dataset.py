fe387910eec4287a624cc4246023b1591db0c2c7
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-07-03 19:28:42 +0200

    added polyglon dataset

---
+++ scripts/datasets/polyglon_dataset.py
@@ -0,0 +1,41 @@
+from ..data.extraction import extract_dataset
+from ..data.preprocessing import data_preprocessing
+from ..datasets.dataset import MyDataset
+import numpy as np
+
+
+class PolyglonDataset(MyDataset):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def load_data(self, filepath):
+        return extract_dataset(filepath)
+
+    def get_x(self, data=None):
+        return data['text'] if data is not None else self.data['text']
+
+    def get_y(self, data=None):
+        data_test = self.get_x()
+        return [0 for i in range(len(data_test))]
+
+    def training_preprocessing(self):
+        pass
+
+    def test_preprocessing(self):
+        prep_data = data_preprocessing(self.data,
+                                       'text',
+                                       norm_contractions=False,
+                                       norm_charsequences=False,
+                                       twitter=False,
+                                       links=True,
+                                       norm_whitespaces=True,
+                                       punctuations=False,
+                                       lowering=False,
+                                       stemming=False,
+                                       lemmatization=False,
+                                       stop_words=True)
+
+        return prep_data
+
+    def postprocessing(self, prediction, model_name):
+        return prediction
