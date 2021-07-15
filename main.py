6402440d64f79e12a702106047a4fe5a8e988c71
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-07-15 22:56:38 +0200

    Remove index.app

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = MultiApp()
 st.title("Stock Manager")
 st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
-app.add_app("Home", index.app)
+# app.add_app("Home", index.app)
 app.add_app("Sentiment Analysis", sentiment_analysis_page.app)
 
 app.run()
